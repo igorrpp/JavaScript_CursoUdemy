816aebb42c4caa64dc9e3322d3d0d9965b9afeee
Igor Rodrigues <igorpires_rj@hotmail.com>
2025-09-06 23:07:44 -0300

    duplicando resultado

--- Fundamentos2.js/atribuicao.js
+++ Fundamentos2.js/atribuicao.js
@@ -6,6 +6,8 @@ console.log(`O resultado é ${b}`)
 
 b -= 4 // b = b - 4
 console.log(`O resultador é ${b}`)
+console.log(`O resultador é ${b}`)
+console.log(`O resultador é ${b}`)
 
 b *= 2 // b = b * 2
 console.log(b)
